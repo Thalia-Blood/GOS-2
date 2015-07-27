8dca204435b99b8ca43aa2f637828a5d569554cf
TsuTsuTsu <marci.sarkany@gmail.com>
2015-07-27 20:01:10 +0200

    v12

--- leblanc.lua
+++ leblanc.lua
@@ -1,34 +1,32 @@
 require('Inspired')
 
-AddButton("Q", "Use [Q] in Combo", true)
-AddButton("W", "Use [W] in Combo", true)
-AddButton("E", "Use [E] in Combo", true)
-AddButton("R", "Use [R] in Combo", true)
-AddKey("Combo", "Do Combo", string.byte(" "))
-AddKey("LastHit", "Do LastHit", string.byte("X"))
-AddKey("LaneClear", "Do LaneClear", string.byte("V"))
+Menu = scriptConfig("sterlingyi", "Sterling Yi")
+	Menu.addParam("Q", "Use Q in Combo", SCRIPT_PARAM_ONOFF, true)
+	Menu.addParam("W", "Use W in Combo", SCRIPT_PARAM_ONOFF, true)
+	Menu.addParam("E", "Use E in Combo", SCRIPT_PARAM_ONOFF, true)
+	Menu.addParam("R", "Use R in Combo", SCRIPT_PARAM_ONOFF, true)
+	Menu.addParam("combo", "Combo Key", SCRIPT_PARAM_KEYDOWN, string.byte(" "))
 	
 local myHero = GetMyHero()
 
 OnLoop(function(myHero)
-	DrawMenu()
 	myHeroPos = GetOrigin(myHero)
 	local target = GetCurrentTarget()
-	if GetKeyValue("Combo") then
+	if GetKeyValue("combo") then
 	
-	    	if CanUseSpell(myHero, _Q) == READY and GetButtonValue("Q") then
+	    	if CanUseSpell(myHero, _Q) == READY and Menu.Q then
 			if ValidTarget(target, GetCastRange(myHero, _Q)) then
 				CastTargetSpell(target, _Q)
 			end
 		end
 		
-		 if CanUseSpell(myHero, _R) == READY and GetButtonValue("R") then
+		 if CanUseSpell(myHero, _R) == READY and Menu.R then
 			if ValidTarget(target, GetCastRange(myHero, _R)) then
 				CastTargetSpell(target, _R)
 			end
 		end
 		
-		if CanUseSpell(myHero, _W) == READY and GetButtonValue("W") and GetCastName(myHero, _W) ~= "leblancslidereturn" then
+		if CanUseSpell(myHero, _W) == READY and Menu.W and GetCastName(myHero, _W) ~= "leblancslidereturn" then
 			if ValidTarget(target, GetCastRange(myHero, _W)) then
 				local WPred = GetPredictionForPlayer(myHeroPos, target, GetMoveSpeed(target), 1500, 0, GetCastRange(myHero, _Q), 220, false, false)
 				if WPred.HitChance == 1 then
@@ -37,7 +35,7 @@ OnLoop(function(myHero)
 			end
 		end
 		
-		if CanUseSpell(myHero, _E) == READY and GetButtonValue("E") then
+		if CanUseSpell(myHero, _E) == READY and Menu.E then
 			if ValidTarget(target, GetCastRange(myHero, _E-25)) then 
 				local EPred = GetPredictionForPlayer(myHeroPos, target, GetMoveSpeed(target), 1600, 250, GetCastRange(myHero, _E), 70, true, true)				
 				if EPred.HitChance == 1 then
