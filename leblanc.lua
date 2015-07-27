e150bdd783e7b6a2e866215009ac377ec6d2f800
TsuTsuTsu <marci.sarkany@gmail.com>
2015-07-27 20:09:28 +0200

    Update leblanc.lua

--- leblanc.lua
+++ leblanc.lua
@@ -1,6 +1,6 @@
 require('Inspired')
 
-Menu = scriptConfig("lb", "Sterling LB")
+Menu = scriptConfig("lb", "Sterling LeBlanc")
 	Menu.addParam("Q", "Use Q in Combo", SCRIPT_PARAM_ONOFF, true)
 	Menu.addParam("W", "Use W in Combo", SCRIPT_PARAM_ONOFF, true)
 	Menu.addParam("E", "Use E in Combo", SCRIPT_PARAM_ONOFF, true)
