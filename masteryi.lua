c47d9814bdf96f98673785f60675cca3707e28fe
TsuTsuTsu <marci.sarkany@gmail.com>
2015-07-27 20:15:26 +0200

    v12

--- masteryi.lua
+++ masteryi.lua
@@ -1,22 +1,22 @@
 require("Inspired")
 Menu = scriptConfig("sterlingyi", "Sterling Yi")
-	Menu.addParam("q", "Use Q in Combo", SCRIPT_PARAM_ONOFF, true)
-	Menu.addParam("e", "Use E in Combo", SCRIPT_PARAM_ONOFF, true)
-	Menu.addParam("r", "Use R in Combo", SCRIPT_PARAM_ONOFF, true)
+	Menu.addParam("Q", "Use Q in Combo", SCRIPT_PARAM_ONOFF, true)
+	Menu.addParam("E", "Use E in Combo", SCRIPT_PARAM_ONOFF, true)
+	Menu.addParam("R", "Use R in Combo", SCRIPT_PARAM_ONOFF, true)
 	Menu.addParam("combo", "Combo Key", SCRIPT_PARAM_KEYDOWN, string.byte(" "))
 
 local myHero = GetMyHero()
 
 OnLoop(function(myHero)
-	if CanUseSpell(myHero, _Q) == READY and Menu.combo then
+	if CanUseSpell(myHero, _Q) == READY and Menu.combo and Menu.Q
 		target = GetCurrentTarget()
-		if ValidTarget(target, 599) then CastTargetSpell(target, _Q) end
+		if ValidTarget(target, 600) then CastTargetSpell(target, _Q) end
 	end
 end)
 
 OnProcessSpell(function(unit, spell)
 	if unit == myHero and Menu.combo and spell.name:find("Attack") then		
-		if Menu.e and CanUseSpell(myHero, _E) == READY then CastTargetSpell(myHero, _E) end
-		if Menu.r and CanUseSpell(myHero, _R) == READY then CastTargetSpell(myHero, _R) end		
+		if Menu.E and CanUseSpell(myHero, _E) == READY then CastTargetSpell(myHero, _E) end
+		if Menu.R and CanUseSpell(myHero, _R) == READY then CastTargetSpell(myHero, _R) end		
 	end
 end)
